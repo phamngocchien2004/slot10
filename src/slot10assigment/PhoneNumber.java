ce86fcc2769aaf966616743e8a9147f579a60a02
phamngocchien2004@@ <114908347+phamngocchien2004@users.noreply.github.com>
2023-05-11 14:40:56 +0700

    bt

---
+++ src/slot10assigment/PhoneNumber.java
@@ -0,0 +1,29 @@
+package slot10assigment;
+
+import java.util.ArrayList;
+
+public final class PhoneNumber {
+    private String name;
+    private ArrayList<String> phone;
+
+    public PhoneNumber(String name, String phone) {
+        this.name = name;
+        this.phone = new ArrayList<>();
+        this.phone.add(phone);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getPhone() {
+        return phone;
+    }
+
+}
+
